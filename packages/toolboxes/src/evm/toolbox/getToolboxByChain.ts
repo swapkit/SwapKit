384331e84442bcf9019452cb5452510692769484
61591 <61591uvt@gmail.com>
2025-03-31 23:15:58 +0200

    Update getToolboxByChain.ts

--- packages/toolboxes/src/evm/toolbox/getToolboxByChain.ts
+++ packages/toolboxes/src/evm/toolbox/getToolboxByChain.ts
@@ -1,34 +1,40 @@
 import { Chain } from "@swapkit/helpers";
 
-import { ARBToolbox, AVAXToolbox, BASEToolbox, BSCToolbox, ETHToolbox, MATICToolbox } from "./evm";
+import { ARBToolbox, AURORAToolbox, AVAXToolbox, BASEToolbox, BSCToolbox, ETHToolbox, GNOToolbox, MATICToolbox } from "./evm";
 import { OPToolbox } from "./op";
 
 type ToolboxType = {
   ARB: typeof ARBToolbox;
+  AURORA: typeof AURORAToolbox;
   AVAX: typeof AVAXToolbox;
   BASE: typeof BASEToolbox;
   BSC: typeof BSCToolbox;
   ETH: typeof ETHToolbox;
+  GNO: typeof GNOToolbox;
   MATIC: typeof MATICToolbox;
   OP: typeof OPToolbox;
 };
 
 export const getToolboxByChain = <T extends keyof ToolboxType>(chain: T): ToolboxType[T] => {
   switch (chain) {
-    case Chain.Avalanche:
-      return AVAXToolbox as ToolboxType[T];
     case Chain.Arbitrum:
       return ARBToolbox as ToolboxType[T];
+    case Chain.Aurora:
+      return AURORAToolbox as ToolboxType[T];
+    case Chain.Avalanche:
+      return AVAXToolbox as ToolboxType[T];
     case Chain.Base:
       return BASEToolbox as ToolboxType[T];
-    case Chain.Optimism:
-      return OPToolbox as ToolboxType[T];
-    case Chain.Polygon:
-      return MATICToolbox as ToolboxType[T];
     case Chain.BinanceSmartChain:
       return BSCToolbox as ToolboxType[T];
     case Chain.Ethereum:
       return ETHToolbox as ToolboxType[T];
+    case Chain.Gnosis:
+      return GNOToolbox as ToolboxType[T]:
+    case Chain.Optimism:
+      return OPToolbox as ToolboxType[T];
+    case Chain.Polygon:
+      return MATICToolbox as ToolboxType[T];
     default:
       throw new Error(`Chain ${chain} is not supported`);
   }
