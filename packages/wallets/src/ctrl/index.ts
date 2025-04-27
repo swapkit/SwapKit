0ed65928328382e53602cd19087f350231b360b2
61591 <61591uvt@gmail.com>
2025-04-27 14:24:21 +0200

    typo

--- packages/wallets/src/ctrl/index.ts
+++ packages/wallets/src/ctrl/index.ts
@@ -117,7 +117,7 @@ async function getWalletMethods(chain: (typeof CTRL_SUPPORTED_CHAINS)[number]) {
     case Chain.Aurora:
     case Chain.Avalanche:
     case Chain.Base:
-    case Chain.Berachain,
+    case Chain.Berachain:
     case Chain.BinanceSmartChain:
     case Chain.Ethereum:
     case Chain.Gnosis:
