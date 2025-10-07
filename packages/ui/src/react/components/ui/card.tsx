06b9a093f2514049d7a6494616f6e056741b0428
Daniel Szczepanik <tmlm18@gmail.com>
2025-10-07 16:10:17 +0200

    fix: update import paths for components/ui

--- packages/ui/src/react/components/ui/card.tsx
+++ packages/ui/src/react/components/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "~/lib/utils";
+import { cn } from "../../../lib/utils";
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div className={cn("rounded-xl bg-card text-card-foreground shadow-sm", className)} ref={ref} {...props} />
