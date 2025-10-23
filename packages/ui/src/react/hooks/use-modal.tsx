e0be6e73f50d55cbd3a2614b5fcdd11e7f4dcbc1
Daniel Szczepanik <tmlm18@gmail.com>
2025-10-23 14:27:10 +0200

    feat: improve useModal to work better with dynamic types

--- packages/ui/src/react/hooks/use-modal.tsx
+++ packages/ui/src/react/hooks/use-modal.tsx
@@ -1,26 +1,46 @@
 import { createContext, useContext, useState } from "react";
 import { create } from "zustand";
 
-export const useModalStore = create<{
+type ModalStore = {
   modals: {
     id: string;
     component: React.ReactNode;
     _promise: {
-      promise: Promise<{ confirmed: boolean; data?: unknown }>;
-      resolve: (value?: { confirmed: boolean; data?: unknown }) => void;
+      promise: Promise<{ confirmed: true; data: unknown } | { confirmed: false; data?: never }>;
+      resolve: <T>(value?: { confirmed: true; data: T } | { confirmed: false; data?: never }) => void;
     };
   }[];
-  addModal: <T = unknown>(component: React.ReactNode) => Promise<{ confirmed: boolean; data?: T }>;
+  addModal: <T>(
+    component: React.ReactNode,
+  ) => Promise<{ confirmed: true; data: T } | { confirmed: false; data?: never }>;
   removeModal: (id: string) => void;
-}>((set) => ({
-  addModal<T = unknown>(component: React.ReactNode): Promise<{ confirmed: boolean; data?: T }> {
-    const { resolve, promise } = Promise.withResolvers<{ confirmed: boolean }>();
+};
+
+export const useModalStore = create<ModalStore>((set) => ({
+  addModal<T>(component: React.ReactNode) {
+    const { resolve, promise } = Promise.withResolvers<
+      { confirmed: true; data: T } | { confirmed: false; data?: never }
+    >();
 
     set((state) => {
-      return { modals: [...state.modals, { _promise: { promise, resolve }, component, id: crypto.randomUUID() }] };
+      return {
+        modals: [
+          ...state.modals,
+          {
+            _promise: {
+              promise,
+              resolve: resolve as <T>(
+                value?: { confirmed: true; data: T } | { confirmed: false; data?: never },
+              ) => void,
+            },
+            component,
+            id: crypto.randomUUID(),
+          },
+        ],
+      };
     });
 
-    return promise as Promise<{ confirmed: boolean; data?: T }>;
+    return promise;
   },
   modals: [],
   removeModal: (id: string) => set((state) => ({ modals: state.modals.filter((m) => m.id !== id) })),
@@ -33,14 +53,19 @@ export function useModal<T = unknown>() {
 
   const modal = useModalStore.getState().modals.find((m) => m.id === id);
 
-  const resolve = ({ confirmed, data }: { confirmed: boolean; data?: T }) => {
-    modal?._promise?.resolve?.({ confirmed, data });
+  function resolve({ confirmed, data }: { confirmed: true; data: T } | { confirmed: false; data?: never }) {
+    if (!confirmed) {
+      modal?._promise?.resolve?.({ confirmed: false, data: undefined });
+    } else {
+      modal?._promise?.resolve?.({ confirmed: true, data });
+    }
+
     setIsOpen(false);
 
     setTimeout(() => {
       removeModal({ id });
     }, 200);
-  };
+  }
 
   return {
     defaultOpen,
@@ -57,7 +82,9 @@ export function useModal<T = unknown>() {
   };
 }
 
-export function showModal<T = unknown>(component: React.ReactNode): Promise<{ confirmed: boolean; data?: T }> {
+export function showModal<T = unknown>(
+  component: React.ReactNode,
+): Promise<{ confirmed: true; data: T } | { confirmed: false; data?: never }> {
   return useModalStore.getState().addModal<T>(component);
 }
 
