695b30824bdb44c77d04e89922453edbd331b514
Andrei Charkas <andrei.charkas@leverx.com>
2025-11-04 17:43:53 +0300

    change nginx

--- playgrounds/vite/vite.config.ts
+++ playgrounds/vite/vite.config.ts
@@ -7,7 +7,7 @@ import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SwapKit",
+  base: "/",
 
   build: {
     commonjsOptions: { transformMixedEsModules: true },
