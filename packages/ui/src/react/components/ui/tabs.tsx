93d9a2051963c6c79053710efd9ccedd6708e973
Daniel Szczepanik <tmlm18@gmail.com>
2025-11-13 16:03:13 +0100

    feat: add shadcn/ui components for implementing rich forms

--- packages/ui/src/react/components/ui/tabs.tsx
+++ packages/ui/src/react/components/ui/tabs.tsx
@@ -9,7 +9,7 @@ const tabsTriggerVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 font-medium text-sm ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent data-[state=active]:text-foreground data-[state=active]:shadow-sm",
   {
     defaultVariants: { variant: "stepper" },
-    variants: { variant: { stepper: "h-1 flex-auto bg-white/[0.16] p-0 data-[state=active]:bg-white/[0.92]" } },
+    variants: { variant: { stepper: "h-1 flex-auto bg-white/[0.16] p-0 data-[state=active]:bg-accent" } },
   },
 );
 
