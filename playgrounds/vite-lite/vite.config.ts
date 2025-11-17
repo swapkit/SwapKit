5c1aba8f2217035a301aafa61e46bcc69c6b4675
Daniel Szczepanik <tmlm18@gmail.com>
2025-11-17 13:56:13 +0100

    feat: downgrade vite-node-polyfill-plugin to 0.17.0, update other deps

--- playgrounds/vite-lite/vite.config.ts
+++ playgrounds/vite-lite/vite.config.ts
@@ -4,13 +4,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      // output: { manualChunks: { react: ["react", "react-dom"] } },
-      plugins: [nodePolyfills({ globals: { Buffer: true, global: true, process: true } })],
-    },
-  },
   esbuild: { jsx: "automatic", jsxDev: false },
   plugins: [react(), nodePolyfills({ globals: { Buffer: true, global: true, process: true } })],
-  resolve: { dedupe: ["react", "react-dom"] },
 });
